67462705ba861b7af65a3f062ecaff27d65e495b
Charles Humangit <charlesian10@gmail.com>
2025-05-02 21:49:16 +0800

    Feat: set anchors and docking for dynamic UI scaling

--- Serial Monitor/Form1.Designer.cs
+++ Serial Monitor/Form1.Designer.cs
@@ -1,4 +1,4 @@
-namespace Serial_Monitor
+﻿namespace Serial_Monitor
 {
     partial class Form1
     {
@@ -63,6 +63,8 @@ namespace Serial_Monitor
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.BackColor = System.Drawing.Color.DarkGray;
             this.groupBox1.Controls.Add(this.cBPortName);
             this.groupBox1.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -70,24 +72,28 @@ namespace Serial_Monitor
             this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.groupBox1.Size = new System.Drawing.Size(219, 79);
+            this.groupBox1.Size = new System.Drawing.Size(184, 79);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Port Name";
             // 
             // cBPortName
             // 
+            this.cBPortName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.cBPortName.BackColor = System.Drawing.Color.WhiteSmoke;
             this.cBPortName.Font = new System.Drawing.Font("Lucida Fax", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cBPortName.FormattingEnabled = true;
             this.cBPortName.Location = new System.Drawing.Point(7, 29);
             this.cBPortName.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.cBPortName.Name = "cBPortName";
-            this.cBPortName.Size = new System.Drawing.Size(200, 40);
+            this.cBPortName.Size = new System.Drawing.Size(165, 40);
             this.cBPortName.TabIndex = 0;
             // 
             // groupBox2
             // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.BackColor = System.Drawing.Color.DarkGray;
             this.groupBox2.Controls.Add(this.cBBaudRate);
             this.groupBox2.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -95,13 +101,15 @@ namespace Serial_Monitor
             this.groupBox2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.groupBox2.Size = new System.Drawing.Size(219, 79);
+            this.groupBox2.Size = new System.Drawing.Size(184, 79);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Baudrate";
             // 
             // cBBaudRate
             // 
+            this.cBBaudRate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.cBBaudRate.BackColor = System.Drawing.Color.WhiteSmoke;
             this.cBBaudRate.Font = new System.Drawing.Font("Lucida Fax", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cBBaudRate.FormattingEnabled = true;
@@ -118,7 +126,7 @@ namespace Serial_Monitor
             this.cBBaudRate.Location = new System.Drawing.Point(7, 29);
             this.cBBaudRate.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.cBBaudRate.Name = "cBBaudRate";
-            this.cBBaudRate.Size = new System.Drawing.Size(200, 40);
+            this.cBBaudRate.Size = new System.Drawing.Size(165, 40);
             this.cBBaudRate.TabIndex = 0;
             // 
             // pnlLeftInterface
@@ -135,12 +143,13 @@ namespace Serial_Monitor
             this.pnlLeftInterface.Controls.Add(this.groupBox2);
             this.pnlLeftInterface.Location = new System.Drawing.Point(6, 6);
             this.pnlLeftInterface.Name = "pnlLeftInterface";
-            this.pnlLeftInterface.Size = new System.Drawing.Size(235, 532);
+            this.pnlLeftInterface.Size = new System.Drawing.Size(200, 532);
             this.pnlLeftInterface.TabIndex = 3;
             // 
             // groupBox4
             // 
-            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox4.BackColor = System.Drawing.Color.DarkGray;
             this.groupBox4.Controls.Add(this.lblStatus);
             this.groupBox4.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -148,7 +157,7 @@ namespace Serial_Monitor
             this.groupBox4.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.groupBox4.Size = new System.Drawing.Size(219, 70);
+            this.groupBox4.Size = new System.Drawing.Size(184, 70);
             this.groupBox4.TabIndex = 6;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Status";
@@ -157,20 +166,22 @@ namespace Serial_Monitor
             // 
             this.lblStatus.BackColor = System.Drawing.Color.LightCoral;
             this.lblStatus.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblStatus.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblStatus.Location = new System.Drawing.Point(6, 28);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(207, 37);
+            this.lblStatus.Size = new System.Drawing.Size(172, 37);
             this.lblStatus.TabIndex = 0;
             this.lblStatus.Text = "No Connection";
             this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnDisconnect
             // 
+            this.btnDisconnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.btnDisconnect.BackColor = System.Drawing.Color.DimGray;
             this.btnDisconnect.Location = new System.Drawing.Point(6, 332);
             this.btnDisconnect.Name = "btnDisconnect";
-            this.btnDisconnect.Size = new System.Drawing.Size(219, 75);
+            this.btnDisconnect.Size = new System.Drawing.Size(184, 75);
             this.btnDisconnect.TabIndex = 5;
             this.btnDisconnect.Text = "Disconnect";
             this.btnDisconnect.UseVisualStyleBackColor = true;
@@ -178,9 +189,11 @@ namespace Serial_Monitor
             // 
             // btnRefresh
             // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.btnRefresh.Location = new System.Drawing.Point(13, 4);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(200, 37);
+            this.btnRefresh.Size = new System.Drawing.Size(165, 37);
             this.btnRefresh.TabIndex = 4;
             this.btnRefresh.Text = "Refresh Port Names";
             this.btnRefresh.UseVisualStyleBackColor = true;
@@ -188,10 +201,12 @@ namespace Serial_Monitor
             // 
             // btnConnect
             // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.btnConnect.BackColor = System.Drawing.Color.DimGray;
             this.btnConnect.Location = new System.Drawing.Point(6, 251);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(219, 75);
+            this.btnConnect.Size = new System.Drawing.Size(184, 75);
             this.btnConnect.TabIndex = 3;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -207,9 +222,9 @@ namespace Serial_Monitor
             this.pnlBottomInterface.Controls.Add(this.btnAutoscroll);
             this.pnlBottomInterface.Controls.Add(this.btnTimeStamp);
             this.pnlBottomInterface.Controls.Add(this.btnClearTextBox);
-            this.pnlBottomInterface.Location = new System.Drawing.Point(248, 397);
+            this.pnlBottomInterface.Location = new System.Drawing.Point(210, 397);
             this.pnlBottomInterface.Name = "pnlBottomInterface";
-            this.pnlBottomInterface.Size = new System.Drawing.Size(713, 141);
+            this.pnlBottomInterface.Size = new System.Drawing.Size(872, 141);
             this.pnlBottomInterface.TabIndex = 4;
             // 
             // btnFind
@@ -249,7 +264,7 @@ namespace Serial_Monitor
             // btnClearTextBox
             // 
             this.btnClearTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClearTextBox.Location = new System.Drawing.Point(543, 3);
+            this.btnClearTextBox.Location = new System.Drawing.Point(702, 3);
             this.btnClearTextBox.Name = "btnClearTextBox";
             this.btnClearTextBox.Size = new System.Drawing.Size(163, 37);
             this.btnClearTextBox.TabIndex = 5;
@@ -265,11 +280,11 @@ namespace Serial_Monitor
             this.groupBox3.BackColor = System.Drawing.Color.DarkGray;
             this.groupBox3.Controls.Add(this.richTextBoxOutput);
             this.groupBox3.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(248, 6);
+            this.groupBox3.Location = new System.Drawing.Point(210, 6);
             this.groupBox3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.groupBox3.Size = new System.Drawing.Size(713, 383);
+            this.groupBox3.Size = new System.Drawing.Size(1045, 383);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Text Box";
@@ -278,12 +293,12 @@ namespace Serial_Monitor
             // 
             this.richTextBoxOutput.BackColor = System.Drawing.Color.LightGray;
             this.richTextBoxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBoxOutput.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxOutput.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBoxOutput.Location = new System.Drawing.Point(6, 28);
             this.richTextBoxOutput.Name = "richTextBoxOutput";
             this.richTextBoxOutput.ReadOnly = true;
             this.richTextBoxOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBoxOutput.Size = new System.Drawing.Size(701, 350);
+            this.richTextBoxOutput.Size = new System.Drawing.Size(1033, 350);
             this.richTextBoxOutput.TabIndex = 6;
             this.richTextBoxOutput.Text = "";
             // 
@@ -323,22 +338,23 @@ namespace Serial_Monitor
             // 
             // lstSearchResults
             // 
-            this.lstSearchResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.lstSearchResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lstSearchResults.FormattingEnabled = true;
             this.lstSearchResults.ItemHeight = 22;
             this.lstSearchResults.Location = new System.Drawing.Point(6, 171);
             this.lstSearchResults.Name = "lstSearchResults";
-            this.lstSearchResults.Size = new System.Drawing.Size(273, 356);
+            this.lstSearchResults.Size = new System.Drawing.Size(157, 356);
             this.lstSearchResults.TabIndex = 9;
             this.lstSearchResults.SelectedIndexChanged += new System.EventHandler(this.lstSearchResults_SelectedIndexChanged);
             // 
             // txtSearch
             // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txtSearch.Location = new System.Drawing.Point(6, 28);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(273, 30);
+            this.txtSearch.Size = new System.Drawing.Size(157, 30);
             this.txtSearch.TabIndex = 8;
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
